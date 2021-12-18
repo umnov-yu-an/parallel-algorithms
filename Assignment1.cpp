1ff2f495c9c48588682e445b4e0f30bd674da094
umnov-yu-an <78300794+umnov-yu-an@users.noreply.github.com>
2021-12-18 22:14:39 +0300

    Add files via upload

---
+++ Assignment1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include <omp.h>
+#include <cstdlib>
+
+using namespace std;
+
+const int a = 0;  //Left border of random values range
+const int b = 1000;  //Right border of random values range
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+
+    //int n = strtol(argv[1], NULL, 10);  Line 19 analogue
+    int n = atoi(argv[1]);
+    int *v = new int[n];
+    
+    for (int i = 0; i < n; i++) {v[i] = rand() % (b - a + 1) + a;}
+
+    //Array printing
+    /*
+    cout << "[ ";
+    for (int i = 0; i < n; i++) {cout << v[i] << " ";}
+    cout << "]" << endl;
+    */
+
+    for (int numThreads = 1; numThreads <= 10; numThreads++) {
+
+        int max_val = a;
+
+        clock_t start = clock();
+
+        #pragma omp parallel for num_threads(numThreads) reduction(max:max_val)
+            for (int i = 0; i < n; i++) {if (v[i] > max_val) max_val = v[i];}
+
+        clock_t finish = clock();
+
+        cout << "Max value = " << max_val << " ";
+        cout << "Threads = " << numThreads << "  ";
+        cout << "Time = " << double(finish - start) / CLOCKS_PER_SEC << endl;
+    }
+
+    delete[] v;
+
+    return 0;
+}
